8e886a2198ee5fdb2e9fbcbc85d3787a862fc7d2
arsalannury <arsiiiland@yahoo.com>
2022-09-08 13:14:15 +0430

    add animation to load page

--- src/components/Games/Games.page.js
+++ src/components/Games/Games.page.js
@@ -1,14 +1,18 @@
-import { useEffect,memo } from "react";
+import { useEffect,memo,useRef } from "react";
 import './games.css';
 
 const GamesPage = () => {
+  const mainRef = useRef(null);
 
     useEffect(() => {
+      mainRef.current.style.animationPlayState = 'running'
     },[]);
 
   return (
     <>
-    
+    <div className="main-game-page" ref={mainRef} >
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit repellendus provident eius neque eveniet libero, magnam, enim reiciendis molestias soluta asperiores quas tenetur vel nesciunt minima odit, dolores consequatur animi?
+    </div>
     </>
   )
 }
