19d61ca6496472e400b12badeeaa941b68d13fac
arsalannury <arsiiiland@yahoo.com>
2022-09-12 23:12:24 +0430

    react-query-train

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import HomePage from "./components/Home/Home.page";
-import {QueryClient,QueryClientProvider} from "react-query";
-import {ReactQueryDevtools} from 'react-query/devtools';
-import {Switch,Route} from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { Switch, Route } from "react-router-dom";
 import GamesPage from "./components/Games/Games.page";
 
 const queryClientProp = new QueryClient();
@@ -10,13 +10,13 @@ const queryClientProp = new QueryClient();
 function App() {
   return (
     <>
-    <QueryClientProvider client={queryClientProp}>
-      <Switch>
-        <Route path={'/'} exact component={HomePage} />
-        <Route path={"/games-page"} component={GamesPage} />
-      </Switch>
-      <ReactQueryDevtools initialIsOpen={false} position={"bottom-left"} />
-    </QueryClientProvider>
+      <QueryClientProvider client={queryClientProp}>
+        <Switch>
+          <Route path={"/"} exact component={HomePage} />
+          <Route path={"/games-page"} component={GamesPage} />
+        </Switch>
+        <ReactQueryDevtools initialIsOpen={false} position={"bottom-left"} />
+      </QueryClientProvider>
     </>
   );
 }
--- src/components/Games/Games.page.js
+++ src/components/Games/Games.page.js
@@ -1,21 +1,24 @@
-import { useEffect,memo,useRef } from "react";
-import './games.css';
+import { useEffect, memo, useRef } from "react";
+import "./games.css";
 
 const GamesPage = () => {
   const mainRef = useRef(null);
 
-    useEffect(() => {
-      mainRef.current.style.animationPlayState = 'running'
-    },[]);
+  useEffect(() => {
+    mainRef.current.style.animationPlayState = "running";
+  }, []);
 
   return (
     <>
-    <div className="main-game-page" ref={mainRef} >
-       Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit repellendus provident eius neque eveniet libero, magnam, enim reiciendis molestias soluta asperiores quas tenetur vel nesciunt minima odit, dolores consequatur animi?
-       <div className="background-move"></div>
-    </div>
+      <div className="main-game-page" ref={mainRef}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
+        repellendus provident eius neque eveniet libero, magnam, enim reiciendis
+        molestias soluta asperiores quas tenetur vel nesciunt minima odit,
+        dolores consequatur animi?
+        <div className="background-move"></div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default memo(GamesPage)
+export default memo(GamesPage);
