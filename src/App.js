4983d1941dcbc855b91d4fbe22c238b7ad7c5bc7
arsalannury <arsiiiland@yahoo.com>
2022-09-15 02:11:04 +0430

    remove prev training files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import "./App.css";
-import HomePage from "./components/Home/Home.page";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { Switch, Route } from "react-router-dom";
-import GamesPage from "./components/Games/Games.page";
-import DetailGamePage from "./components/Games/DetailGame.page";
-import LowScorePage from "./components/LowScore/LowScore.page";
+
 
 const queryClientProp = new QueryClient();
 
@@ -14,10 +11,7 @@ function App() {
     <>
       <QueryClientProvider client={queryClientProp}>
         <Switch>
-          <Route path={"/"} exact component={HomePage} />
-          <Route path={"/games-page"} component={GamesPage} />
-          <Route path={"/detail-game/:id"} component={DetailGamePage} />
-          <Route path={"/low-score"} render={(props) => <LowScorePage {...props} lowIds={[1,2,3,4]} />} />
+        
         </Switch>
         <ReactQueryDevtools initialIsOpen={false} position={"bottom-left"} />
       </QueryClientProvider>
