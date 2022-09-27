2850c6034373fe2dc0478dc64a98f990270de7ad
arsalannury <arsiiiland@yahoo.com>
2022-09-27 12:16:47 +0330

    [refactore] restart all train

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
 import "./App.css";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
-import { Switch, Route } from "react-router-dom";
-import HomePage from "./components/HomePage/HomePage";
-import GamesPage from "./components/Games/GamesPage";
-import AboutUsPage from "./components/AboutUs/AboutUsPage";
-import DetailGamePage from "./components/DetailGame/DetailGamePage";
-import BestGamesPage from "./components/BestGames/BestGamesPage";
-import AddGamePage from "./components/AddGame/AddGamePage";
-
-
-const queryClientProp = new QueryClient();
 
 function App() {
   return (
     <>
-      <QueryClientProvider client={queryClientProp}>
-        <Switch>
-        <Route path={'/'} exact component={HomePage} />
-        <Route path={"/games"} component={GamesPage} />
-        <Route path={"/about-us"} component={AboutUsPage} />
-        <Route path={"/game/:id"} component={DetailGamePage} />
-        <Route path={"/best-games"} render={(props) => <BestGamesPage {...props} ids={[0,1,2 ]} /> } />
-        <Route path={"/add-game"} component={AddGamePage} />
-        </Switch>
-        <ReactQueryDevtools initialIsOpen={false} position={"bottom-left"} />
-      </QueryClientProvider>
+     
     </>
   );
 }
